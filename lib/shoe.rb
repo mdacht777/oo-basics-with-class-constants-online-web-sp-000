ae652c97450a20d8b56659d2ac147f4dcdb7204a
Martin Achtermann <mdacht777@gmail.com>
2020-06-14 18:51:50 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,16 +1,4 @@
 # Make your shoe class here!
 class Shoe
-  attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
-  BRANDS = []
 
-  def cobble
-    puts "Your shoe is as good as new!"
-    @condition="new"
-  end
-
-  def brand=(brand)
-    @brand = brand
-    BRANDS << brand 
-  end 
 end
